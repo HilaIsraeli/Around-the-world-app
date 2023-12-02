fde7f62544c5bf69fe26b477b7d5ef354e498041
hila.israeli <hila@ownbackup.com>
2023-12-02 18:53:19 +0200

    prittier and add world map photos

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,8 +1,13 @@
-import styles from '../Styles/page.module.css'
+import styles from "../Styles/page.module.css";
+import worldMap from "../../photos/world_map.jpg";
+import Image from "next/image";
 
 export default function Header() {
-    return (<div className={styles.main}>
-    <h1 className={styles.center_title}>Around the World App</h1>
-    <h3 className={styles.center_title}>By M&M</h3>
-    </div>)
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.center_title}>Around the World App</h1>
+      <h3 className={styles.center_title}>By M&M</h3>
+      <Image className={styles.center_photo} src={worldMap} alt="worldMap" />
+    </div>
+  );
 }
